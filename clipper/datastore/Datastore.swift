e5486fcf72e93161c79ad090a9c597a209167c9e
Wayne Irwin <wayne.j.irwin@gmail.com>
2019-06-16 13:04:18 +1000

    Datatore class

---
+++ clipper/datastore/Datastore.swift
@@ -0,0 +1,19 @@
+//
+//  Datastore.swift
+//  clipper
+//
+//  Created by Wayne Irwin on 16/6/19.
+//  Copyright © 2019 Wayne Irwin. All rights reserved.
+//
+
+import Foundation
+
+
+class Datastore {
+    static let shared = Datastore()
+    
+    init(){}
+    
+}
+
+
