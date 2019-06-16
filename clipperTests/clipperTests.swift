7d163a434c80083b1ad5aef78637dd9b5aed3e20
Wayne Irwin <wayne.j.irwin@gmail.com>
2019-06-16 12:22:37 +1000

    Initial Commit

---
+++ clipperTests/clipperTests.swift
@@ -0,0 +1,34 @@
+//
+//  clipperTests.swift
+//  clipperTests
+//
+//  Created by Wayne Irwin on 16/6/19.
+//  Copyright © 2019 Wayne Irwin. All rights reserved.
+//
+
+import XCTest
+@testable import clipper
+
+class clipperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
