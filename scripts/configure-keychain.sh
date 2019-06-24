2071129251edc7c272debb07fe9d75c628db8d7f
Wayne Irwin <wayne.j.irwin@gmail.com>
2019-06-25 09:02:40 +1000

    regen a cert

--- scripts/configure-keychain.sh
+++ scripts/configure-keychain.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in resources/development-key.p12.enc -d -a -out resources/development-key.p12
-openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in resources/mac_development.cer.enc -d -a -out resources/mac_development.cer
+openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in resources/mac_app.p12.enc -d -a -out resources/mac_app.p12
 
 
 security create-keychain -p $CUSTOM_KEYCHAIN_PASSWORD macos-build.keychain
@@ -12,8 +11,7 @@ security unlock-keychain -p $CUSTOM_KEYCHAIN_PASSWORD macos-build.keychain
 
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/macos-build.keychain
 
-security import resources/development-key.p12 -k macos-build.keychain -P $SECURITY_PASSWORD -A
-security import resources/mac_development.cer -k macos-build.keychain -A
+security import resources/mac_app.p12 -k macos-build.keychain -P $SECURITY_PASSWORD -A
 
 
 security set-key-partition-list -S apple-tool:,apple: -s -k $SECURITY_PASSWORD macos-build.keychain > /dev/null
