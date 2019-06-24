8f095b36cb2501c595fdebef713faf5f191f0ed3
Wayne Irwin <wayne.j.irwin@gmail.com>
2019-06-25 09:21:23 +1000

    testing

--- scripts/configure-keychain.sh
+++ scripts/configure-keychain.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in resources/mac_app.p12.enc -d -a -out resources/mac_app.p12
+openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in resources/mac_developer.p12.enc -d -a -out resources/mac_developer.p12
 
 
 security create-keychain -p $CUSTOM_KEYCHAIN_PASSWORD macos-build.keychain
@@ -11,7 +11,6 @@ security unlock-keychain -p $CUSTOM_KEYCHAIN_PASSWORD macos-build.keychain
 
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/macos-build.keychain
 
-security import resources/mac_app.p12 -k macos-build.keychain -P $SECURITY_PASSWORD -A
-
+security import resources/mac_developer.p12 -k macos-build.keychain -P $SECURITY_PASSWORD -A
 
 security set-key-partition-list -S apple-tool:,apple: -s -k $SECURITY_PASSWORD macos-build.keychain > /dev/null
